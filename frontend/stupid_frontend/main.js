513054b27e70c0cc8c2b0ad0075d843d7abbb7e1
joaoAaf <joao.anderson.af@gmail.com>
2023-05-23 20:21:53 -0300

    Criação da função addInput e conclusão da updateUserInit

--- frontend/stupid_frontend/main.js
+++ frontend/stupid_frontend/main.js
@@ -27,8 +27,23 @@ function deleteUser(id) {
 }
 
 function updateUserInit(id) {
-    const td = document.getElementById(id+"1")
-    td.innerHTML = "Deu certo!"
+    const tr = document.getElementById(id)
+    let td = document.getElementById(id+"0")
+    td.appendChild(addInput("text","Nome"))
+
+    td = document.getElementById(id+"1")
+    td.appendChild(addInput("email","exemplo@email.com"))
+
+    td = document.getElementById(id+"2")
+    tr.removeChild(td)
+    td = document.createElement('td')
+    td.id = id+"2"
+    let div = document.createElement('div')
+    div.className = "d-grid gap-2 col-6"
+    div.appendChild(addButton("Aplicar", 2, id))
+    div.appendChild(addButton("Cancelar", 3, id))
+    td.appendChild(div)
+    tr.appendChild(td)
 }
 
 function getUsers(data) {
@@ -54,7 +69,8 @@ function getUsers(data) {
         tr.appendChild(td)
 
         td = document.createElement('td')
-        div = document.createElement('div')
+        td.id = user.id+"2"
+        let div = document.createElement('div')
         div.className = "d-grid gap-2 col-6"
         div.appendChild(addButton("Remover", 0, user.id))
         div.appendChild(addButton("Alterar", 1, user.id))
@@ -74,12 +90,27 @@ function addButton(nome, num, id) {
     return button
 }
 
+function addInput (type,placeHolder) {
+    const input = document.createElement('input')
+    input.type = type
+    input.className = "form-control"
+    input.placeholder = placeHolder
+    input.value = ''
+    return input
+}
+
 function click(num, id) {
     if (num == 0) {
         axiosDelete(id)
     }
     else if (num == 1) {
         updateUserInit(id)
+    }
+    else if (num == 2) {
+        alert("Deu certo! /o/")
+    }
+    else if (num == 3) {
+        alert("Deu certo! |o|")
     }
 }
 
